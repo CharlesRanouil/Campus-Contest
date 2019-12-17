0e31f6e52f5b0dc052a3c3cb0860b8b8bb42d108
Jeremy <jeremybrunet86470@gmail.com>
2019-12-17 21:51:22 +0100

    fin de journee

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
         <meta charset="UTF-8">
         <title>Campus Contest</title>
         <link rel="stylesheet" type="text/css" href="index_style.css">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato%3Aregular%2C300%7COpen%20Sans%3A700%2C400&amp;subset=">
     </head>
     <!--Corp du HTML-->
     <body>
@@ -13,6 +16,21 @@
         <!-- menu -->
         <nav>
             <!-- liste à puces -->
+            <ul class="name_left">
+                <h2>
+                    <!-- <a href="index.php" class="title_name" title="Accueil"> -->
+                    <?php
+                        $mysqli->real_query("SELECT nom, prenom, utiliser FROM users WHERE utiliser = 1");
+                        $res = $mysqli->use_result();
+
+                        while ($row = $res->fetch_assoc()) {
+                            echo $row['nom']." ";
+                            echo $row['prenom'];
+                        }
+                    ?>
+                <!-- </a> -->
+            </h2>
+            </ul>
             <ul class="niv_1">
                 <li>
                     <a href="#" class="btn btn-outline-dark btn-lg" title="Acceuil">Accueil</a>
@@ -46,25 +64,44 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quod sint aliquid in dolorem eius quo, laboriosam aspernatur blanditiis reiciendis porro ea labore optio fugit dicta tenetur nesciunt! Odit, quidem?
             
         </p>
-
-        <form method="post" action="update.php">
-            <button type="submit" name="passed" id="passed" class="btn btn-primary">Jerem<i class="fa fa-check"></i></button>
-            <button type="submit" name="insufficient" id="insufficient" class="btn btn-primary">Charles<i class="fa fa-times"></i></button>
-        </form></br>
-    </div>  
-    <div class="txt_intro">
+      
     <?php
         $mysqli->real_query("SELECT nom, prenom, utiliser FROM users ORDER BY id ASC");
         $res = $mysqli->use_result();
 
         while ($row = $res->fetch_assoc()) {
-            echo '<div class="test">';
             echo " Nom = " . $row['nom'] . "\n<br>";
             echo " Prénom = " . $row['prenom'] . "\n<br>";
             echo " Utiliser = " . $row['utiliser'] . "\n<br>";
-            echo "<br></div>";
+            echo "<br>";
         }
     ?>
+    </div>
+    <div class="block_blue">
+        <div class="column_container">
+            <h2>Titre</h2>
+            <h3>Titre 2</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi velit voluptates modi assumenda totam. Voluptate, facilis quos quasi quae illo reprehenderit aperiam tempora quibusdam, consequuntur recusandae nisi nesciunt pariatur voluptatibus.   
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero in, esse vel accusantium quod eum veniam, officia magnam ullam error, ipsum dignissimos quia sunt doloremque! Voluptates veniam quisquam obcaecati sit.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis in necessitatibus nobis distinctio veniam, enim nam animi error magni maxime! Modi fugit explicabo ratione at voluptatibus, fuga voluptates cupiditate voluptas.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magnam consequuntur quaerat saepe in ipsa tempora deserunt, laudantium sunt quos ratione! Voluptate dolorum eos dolorem repellat, dicta corrupti facilis dolor.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsum sed unde voluptatibus itaque dolorum repudiandae culpa commodi asperiores sapiente inventore molestiae rerum sunt eveniet nostrum quisquam, corporis, alias illo!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quod sint aliquid in dolorem eius quo, laboriosam aspernatur blanditiis reiciendis porro ea labore optio fugit dicta tenetur nesciunt! Odit, quidem? 
+            </p>
+        </div>
+        <div class="column_container">
+            <h3>Titre</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi velit voluptates modi assumenda totam. Voluptate, facilis quos quasi quae illo reprehenderit aperiam tempora quibusdam, consequuntur recusandae nisi nesciunt pariatur voluptatibus.   
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero in, esse vel accusantium quod eum veniam, officia magnam ullam error, ipsum dignissimos quia sunt doloremque! Voluptates veniam quisquam obcaecati sit.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis in necessitatibus nobis distinctio veniam, enim nam animi error magni maxime! Modi fugit explicabo ratione at voluptatibus, fuga voluptates cupiditate voluptas.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magnam consequuntur quaerat saepe in ipsa tempora deserunt, laudantium sunt quos ratione! Voluptate dolorum eos dolorem repellat, dicta corrupti facilis dolor.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsum sed unde voluptatibus itaque dolorum repudiandae culpa commodi asperiores sapiente inventore molestiae rerum sunt eveniet nostrum quisquam, corporis, alias illo!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quod sint aliquid in dolorem eius quo, laboriosam aspernatur blanditiis reiciendis porro ea labore optio fugit dicta tenetur nesciunt! Odit, quidem? 
+            </p>
+        </div>
+    </div>
     <!-- <?php  
         $mysqli->real_query("SELECT URL, description FROM image WHERE description='Logo'");
         $res = $mysqli->use_result();
@@ -73,7 +110,6 @@
             echo '<img src="' . $row["URL"] . '" alt="' . $row["description"] . '">';
         }
     ?> -->
-    </div>
     </body>
  <!--footer-->
  <footer>
@@ -82,21 +118,25 @@
         &copy; 2019 - 
             
         <?php
-        $mysqli->real_query("SELECT nom, prenom, utiliser FROM users WHERE utiliser = 1");
-        $res = $mysqli->use_result();
+            $mysqli->real_query("SELECT nom, prenom, utiliser FROM users WHERE utiliser = 1");
+            $res = $mysqli->use_result();
 
-        while ($row = $res->fetch_assoc()) {
-            echo '<a target="_blank" href="#" title="';
-            echo $row['nom']."\n";
-            echo $row['prenom'];
-            echo '">';
-            echo $row['nom']."\n";
-            echo $row['prenom'];
-            echo "</a>";
-        }
-    ?>
+            while ($row = $res->fetch_assoc()) {
+                echo '<a target="_blank" href="#" title="';
+                echo $row['nom']." ";
+                echo $row['prenom'];
+                echo '">';
+                echo $row['nom']."\n";
+                echo $row['prenom'];
+                echo "</a>";
+            }
+        ?>
 
-        </a>
+        </br></br>
+        <form method="post" action="update.php">
+            <button type="submit" name="jeremy" id="jeremy" class="btn btn-primary">Jeremy<i class="fa fa-check"></i></button>
+            <button type="submit" name="charles" id="charles" class="btn btn-primary">Charles<i class="fa fa-times"></i></button>
+        </form>
     </div>
     <!-- droite -->
     <div class="right">
@@ -108,26 +148,3 @@
     </div>
  </footer>
 </html>
-
-<script>
-function FnBookmark(id,mode){
-
-$.ajax({
-                    url:'update.php',
-                    data:{mode:mode,id:id},
-                    dataType:'json',
-                    success:function(data){
-                          if(mode == "on")
-                          {
-                              $("#1").attr("class", "offbutton");
-                              $("#1").attr("onClick","FnBookmark('1','off')");
-                          }
-                          else
-                          {
-                              $("#1").attr("class", "onbutton");
-                              $("#1").attr("onClick","FnBookmark('1','on')");
-
-                          }
-                    }
-      });
-</script>
--- update.php
+++ update.php
@@ -4,22 +4,24 @@
 include 'bdd.php';
 
 
-if(isset($_POST['passed'])){
+if(isset($_POST['jeremy'])){
 
     $utiliser = $mysqli->query("SELECT utiliser FROM users WHERE id = 1");
     $donnee = $utiliser->fetch_assoc();
 
     if ($donnee['utiliser'] == 0) {
         mysqli_query($mysqli,"UPDATE users SET utiliser = 1 WHERE id = 1 ");
+        mysqli_query($mysqli,"UPDATE users SET utiliser = 0 WHERE id = 2 ");
     } 
     
     if ($donnee['utiliser'] == 1) {
         mysqli_query($mysqli,"UPDATE users SET utiliser = 0 WHERE id = 1 ");
+        mysqli_query($mysqli,"UPDATE users SET utiliser = 1 WHERE id = 2 ");
     }
 
 }
 
-if(isset($_POST['insufficient'])){
+if(isset($_POST['charles'])){
 
 
     $utiliser = $mysqli->query("SELECT utiliser FROM users WHERE id = 2");
@@ -27,10 +29,12 @@ if(isset($_POST['insufficient'])){
 
         if ($donnee['utiliser'] == 0) {
             mysqli_query($mysqli,"UPDATE users SET utiliser = 1 WHERE id = 2 ");
+            mysqli_query($mysqli,"UPDATE users SET utiliser = 0 WHERE id = 1 ");
         } 
         
         if ($donnee['utiliser'] == 1) {
             mysqli_query($mysqli,"UPDATE users SET utiliser = 0 WHERE id = 2 ");
+            mysqli_query($mysqli,"UPDATE users SET utiliser = 1 WHERE id = 1 ");
         }
 
 
