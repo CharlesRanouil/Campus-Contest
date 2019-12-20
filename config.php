b5ed4287224bec6f0a3db149cf0117f2f3c41f5f
Jeremy <jeremybrunet86470@gmail.com>
2019-12-20 13:55:15 +0100

    liaison page contact a la BDD

--- config.php
+++ config.php
@@ -12,14 +12,4 @@ connexion à la base de données
 $mysqli = new mysqli($serveur, $user, $pass, $base);
 // si la connexion se fait en UTF-8, sinon ne rien indiquer
 $mysqli->set_charset("utf8");
-/*
-utilisation de la méthode connect_error
-qui renvoie un message d'erreur si la connexion échoue
-*/
-// if ($mysqli->connect_error) {
-//     echo 'connexion impossible... :'.$mysqli->connect_error;
-// }
-// else {
-//     echo 'connexion réussie : '.$mysqli->host_info;
-// }
 ?>
