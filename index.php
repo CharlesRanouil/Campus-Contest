e343f580827f0ddcd68e818fa7956d6a0ba930ad
Jeremy <jeremybrunet86470@gmail.com>
2019-12-20 14:37:50 +0100

    modif

--- index.php
+++ index.php
@@ -21,18 +21,48 @@
         <section>
             <div class="txt_intro" id="nav1">
                 <div class="picture">
-                    <img src="assets/images/jeremy.jfif" alt="image profil" width=315px >
+                    <!-- <img src="assets/images/jeremy.jfif" alt="image profil" width=315px > -->
+                    <?php
+                            $mysqli->real_query("SELECT users.utiliser, presentation.img FROM users INNER JOIN presentation ON users.id = presentation.id_users WHERE users.utiliser = 1 LIMIT 0,25");
+                            $res = $mysqli->use_result();
+
+                            while ($row = $res->fetch_assoc()) {
+                                echo '<img src="';
+                                echo $row['img'];
+                                echo '"alt="image profil" width=315px >';
+                            }
+                        ?>
                 </div>
                 <div class="column_container_right">
-                    <h2>Titre</h2>
-                    <h3>Titre 2</h3>
+                    <h2> <?php
+                            $mysqli->real_query("SELECT nom, prenom, utiliser FROM users WHERE utiliser = 1");
+                            $res = $mysqli->use_result();
+
+                            while ($row = $res->fetch_assoc()) {
+                                echo $row['nom']. " ";
+                                echo $row['prenom'];
+                            }
+                        ?></h2>
+                    <h3>
+                    <?php
+                            $mysqli->real_query("SELECT users.utiliser, presentation.age FROM users INNER JOIN presentation ON users.id = presentation.id_users WHERE users.utiliser = 1 LIMIT 0,25");
+                            $res = $mysqli->use_result();
+
+                            while ($row = $res->fetch_assoc()) {
+                                echo $row['age'];
+                                echo " ans";
+                            }
+                        ?>
+                    </h3>
                     <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi velit voluptates modi assumenda totam. Voluptate, facilis quos quasi quae illo reprehenderit aperiam tempora quibusdam, consequuntur recusandae nisi nesciunt pariatur voluptatibus.   
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero in, esse vel accusantium quod eum veniam, officia magnam ullam error, ipsum dignissimos quia sunt doloremque! Voluptates veniam quisquam obcaecati sit.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis in necessitatibus nobis distinctio veniam, enim nam animi error magni maxime! Modi fugit explicabo ratione at voluptatibus, fuga voluptates cupiditate voluptas.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique magnam consequuntur quaerat saepe in ipsa tempora deserunt, laudantium sunt quos ratione! Voluptate dolorum eos dolorem repellat, dicta corrupti facilis dolor.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsum sed unde voluptatibus itaque dolorum repudiandae culpa commodi asperiores sapiente inventore molestiae rerum sunt eveniet nostrum quisquam, corporis, alias illo!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quod sint aliquid in dolorem eius quo, laboriosam aspernatur blanditiis reiciendis porro ea labore optio fugit dicta tenetur nesciunt! Odit, quidem? 
+                    <?php
+                            $mysqli->real_query("SELECT users.utiliser, presentation.description FROM users INNER JOIN presentation ON users.id = presentation.id_users WHERE users.utiliser = 1 LIMIT 0,25");
+                            $res = $mysqli->use_result();
+
+                            while ($row = $res->fetch_assoc()) {
+                                echo $row['description'];
+                            }
+                        ?>
                     </p>
                 </div>
             </div>
